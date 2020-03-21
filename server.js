0074c3fb15176f3185bf689a869c4ef3f6efd5bc
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-03-21 10:58:20 -0700

    add process.env

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ var fs = require("fs");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.port || 3000;
+
+
+
+
+
 
 var notes = [];
 
